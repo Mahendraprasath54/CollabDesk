54fade018f4b4221328a9562e8f82945d78a56ff
Mahendra prasath.s <mahendraprasath.553@gmail.com>
2026-04-15 17:14:50 +0530

    schema for user task and team created

---
+++ collabdesk-backend/models/Task.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose")
+
+const taskSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  status: {
+    type: String,
+    enum: ["todo", "in-progress", "done"],
+    default: "todo"
+  },
+  assignedTo: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
+  },
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
+  },
+  team: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Team"
+  },
+  dueDate: Date
+}, { timestamps: true })
+
+module.exports = mongoose.model("Task", taskSchema)
---
+++ collabdesk-backend/models/User.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose")
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: { type: String, unique: true },
+  password: String,
+  role: {
+    type: String,
+    enum: ["member", "admin"],
+    default: "member"
+  },
+  team: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Team"
+  }
+}, { timestamps: true })
+
+module.exports = mongoose.model("User", userSchema)
