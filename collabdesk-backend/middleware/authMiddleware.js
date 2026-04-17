1faa9ebb88bc5dadf85a7247197f047452ca1a0a
Mahendra prasath.s <mahendraprasath.553@gmail.com>
2026-04-17 10:36:18 +0530

    modified miidleware to split token from the header

--- collabdesk-backend/middleware/authMiddleware.js
+++ collabdesk-backend/middleware/authMiddleware.js
@@ -2,9 +2,11 @@ const jwt = require("jsonwebtoken")
 const User = require("../models/User")
 
 module.exports = async (req, res, next) => {
-  const token = req.headers.authorization
+  const authHeader = req.headers.authorization
+  if (!authHeader || !authHeader.startsWith("Bearer "))
+    return res.status(401).json({ msg: "No token" })
 
-  if (!token) return res.status(401).json({ msg: "No token" })
+  const token = authHeader.split(" ")[1]
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET)
